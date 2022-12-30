929fc61eb0fb6fadf8bf128c330d194f6488b0c6
Hunter-Borkowski <hunter.borkowski96@gmail.com>
2022-12-30 01:24:27 -0500

    Created eventListener to seekbar and added dynamic percent label

--- app/src/main/java/com/hborkowski/tippy/MainActivity.java
+++ app/src/main/java/com/hborkowski/tippy/MainActivity.java
@@ -3,12 +3,49 @@ package com.hborkowski.tippy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private String TAG = "MainActivity";
+    private EditText etBaseAmount;
+    private SeekBar seekBarTip;
+    private TextView tvTipPercentLabel;
+    private TextView tvTipAmount;
+    private TextView tvTotalAmount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etBaseAmount = findViewById(R.id.etBaseAmount);
+        seekBarTip = findViewById(R.id.seekBarTip);
+        tvTipPercentLabel = findViewById(R.id.tvTipPercentLabel);
+        tvTipAmount = findViewById(R.id.tvTipAmount);
+        tvTotalAmount = findViewById(R.id.tvTotalAmount);
+
+
+        seekBarTip.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i(TAG, "onProgress Changed " + i);
+                tvTipPercentLabel.setText(Integer.toString(i));
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
